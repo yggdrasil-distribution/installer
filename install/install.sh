21e2478f1cc2c10b7012bb785d7376f576aec91f
Francesco Favale <francesco@favale.eu>
2022-12-01 16:53:06 +0100

    Checks if the script is being run within certain parameters

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -e
+
+# This file is part of the Yggdrasil Installer.
+#   the Yggdrasil Installer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#   the Yggdrasil Installer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#   You should have received a copy of the GNU General Public License along with the Yggdrasil Installer. If not, see <https://www.gnu.org/licenses/>.
+
 #  #### ##    ##  ######  ########    ###    ##       ##
 #   ##  ###   ## ##    ##    ##      ## ##   ##       ##
 #   ##  ####  ## ##          ##     ##   ##  ##       ##
@@ -19,12 +25,23 @@ clear && printf "\
  Congratulations! You have begun the installation of Yggdrasil.\n"
 sleep 1
 
-if [ "$(basename "$(pwd)")" = "yggdrasil_installer" ]; then
-  echo ""
-else
-  echo "Please cd into the \"yggdrasil_installer\" directory to start the installation in order to avoid any unfortunate mishaps."
-  exit 1
-fi
+is_correct_dir () {
+  if [ "$(basename "$(pwd)")" != "yggdrasil_installer" ]; then
+    echo "Please cd into the \"yggdrasil_installer\" directory to start the installation in order to avoid any unfortunate mishaps."
+    exit 1
+  fi
+}
+
+is_root_user () {
+  if [ "$(whoami)" != "root" ]; then
+    echo "Please run the installation script as root"
+    exit 1
+  fi
+}
+
+# Check if the requirements are met to complete the installation properly
+is_root_user
+is_correct_dir
 
 mkdir -pv "/mnt/gentoo"
 
